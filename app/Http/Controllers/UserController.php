c8105c5580539a76dce02cf6413d7944f99faaae
yusei-iwaki <iwaki.yusei11@gmail.com>
2025-02-17 00:02:06 +0900

    コントローラ作成

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+
+        return view('users.index');
+    }
+
+    public function show()
+    {
+        return view('users.show');
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            新着投稿
+        </h2>
+    </x-slot>
+</x-app-layout>
