ecdd145524879b15243721b2f57f6babdee835ba
yusei-iwaki <iwaki.yusei11@gmail.com>
2025-02-18 00:18:26 +0900

    ナビへリンク追加 ・新規作成 ・新着投稿 ・オーナー

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,15 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('posts.create')" :active="request()->routeIs('posts.create')">
+                        新規作成
+                    </x-nav-link>
+                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
+                        新着投稿
+                    </x-nav-link>
+                    <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
+                        オーナー
+                    </x-nav-link>
                 </div>
             </div>
 
